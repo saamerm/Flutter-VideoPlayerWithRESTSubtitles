1b38054d3c103375a9bdd0836bd3d5692cdabfa5
Saamer Mansoor <i@saamer.me>
2019-10-14 16:39:41 -0400

    Adding favorited icons to the list

--- video_with_subtitles/lib/main.dart
+++ video_with_subtitles/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
   
   @override
@@ -42,10 +43,15 @@ class RandomWordsState extends State<RandomWords> {
   }
   
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
