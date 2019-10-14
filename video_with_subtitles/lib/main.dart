69f9bcf3b4716f80ab7196ad09362107cf9f6be7
Saamer Mansoor <i@saamer.me>
2019-10-14 15:42:45 -0400

    Added a stateful widget

--- video_with_subtitles/lib/main.dart
+++ video_with_subtitles/lib/main.dart
@@ -5,8 +5,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();            
+  Widget build(BuildContext context) {         
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -14,9 +13,21 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),            
+          child: RandomWords(),            
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
