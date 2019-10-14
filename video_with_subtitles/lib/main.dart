dd90a84e251b93a0dc9ce9a81f112d69b0b21b14
Saamer Mansoor <i@saamer.me>
2019-10-14 15:37:17 -0400

    Using 3rd party dart package
    * From pub.dev, found n added a package
    * Using 3rd party dart package in solution

--- video_with_subtitles/lib/main.dart
+++ video_with_subtitles/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();            
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello Worlds'),
+          child: Text(wordPair.asPascalCase),            
         ),
       ),
     );
