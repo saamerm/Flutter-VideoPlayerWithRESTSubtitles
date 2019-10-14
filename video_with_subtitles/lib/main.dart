47f1c75437c2db87378b329383b781847b4d872b
Saamer Mansoor <i@saamer.me>
2019-10-14 17:33:33 -0400

    Update the theme color

--- video_with_subtitles/lib/main.dart
+++ video_with_subtitles/lib/main.dart
@@ -7,6 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {         
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.grey,
+      ),
       home: RandomWords(),            
     );
   }
